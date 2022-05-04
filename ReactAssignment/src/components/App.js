7995debfe364179e43eb60d6cbae049217b5e3c2
prabhattopi <topi9864@gmail.com>
2022-05-05 02:59:20 +0530

    uuir847ui

---
+++ ReactAssignment/src/components/App.js
@@ -0,0 +1,15 @@
+import React,{ Component} from "react";
+
+class App extends Component{ 
+   render() {
+     return(
+        <div>
+         <h1>Hello World Ramesh ki Maa ka</h1>
+        </div>
+     ) 
+        
+     
+   }
+}
+
+export default App;
